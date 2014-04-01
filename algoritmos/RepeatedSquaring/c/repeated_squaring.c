d9eacd12eab4df2949314e02b5af3f83af80ce3e
João Júnior <joaojunior_ma@gmail.com>
2014-04-01 20:46:18 -0300

    Implementação do algoritmo Faster-All-Pairs-Shortest-Paths

---
+++ algoritmos/RepeatedSquaring/c/repeated_squaring.c
@@ -0,0 +1,35 @@
+#include "repeated_squaring.h"
+Graph *repeated_squaring(Graph *graph){
+    Graph *result;
+    int m;
+    result = (Graph *)malloc(sizeof(Graph));
+    result->numbers_nodes = graph->numbers_nodes;
+    allocate_memory(result);
+    for(int i = 0; i < result->numbers_nodes; i++)
+        for(int j = 0; j < result->numbers_nodes; j++)
+            if(i == j or get_cost_edge(graph, i, j) != 0)
+                insert_arc(result, i, j, get_cost_edge(graph, i, j));
+            else
+                insert_arc(result, i, j, MAX_DISTANCE);
+    m = 1;
+    while(m < graph->numbers_nodes - 1){
+        result = extend(result, result);
+        m = 2 * m;
+    }
+    return result;
+};
+
+Graph *extend(Graph *L, Graph *W){
+    int l;
+    for(int i = 0; i < L->numbers_nodes; i++){
+        for(int j = 0; j < L->numbers_nodes; j++){
+            l = MAX_DISTANCE;
+            for(int k = 0; k < L->numbers_nodes; k++){
+                if(get_cost_edge(L, i, k) + get_cost_edge(W, k, j) < l)
+                    l = get_cost_edge(L, i, k) + get_cost_edge(W, k, j);
+            }
+            L->edges[i][j] = l;
+        }
+    }
+    return L;
+};
---
+++ algoritmos/RepeatedSquaring/c/repeated_squaring.h
@@ -0,0 +1,13 @@
+#include <stddef.h>
+#include <stdlib.h>
+#ifndef REPEATED_SQUARING_H
+#define REPEATED_SQUARING_H
+#define MAX_DISTANCE 100000
+#include "../../../estrutura_dados/grafo_matriz_adjacencia/c/graph.h"
+#include "../../../estrutura_dados/fila/c/fila.h"
+
+Graph *repeated_squaring(Graph *);
+Graph *extend(Graph *, Graph *);
+#endif
+
+
