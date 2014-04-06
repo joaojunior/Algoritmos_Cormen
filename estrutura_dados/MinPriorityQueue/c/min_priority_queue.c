c80013c6735490a953dff49396f650b31cd0109b
João Júnior <joaojunior_ma@gmail.com>
2014-04-06 15:39:09 -0300

    Refatorando a fila de prioridades mínima

--- estrutura_dados/MinPriorityQueue/c/min_priority_queue.c
+++ estrutura_dados/MinPriorityQueue/c/min_priority_queue.c
@@ -6,20 +6,19 @@ int minimum(MinPriorityQueue *min_priority_queue){
 int extract_minimum(MinPriorityQueue *min_priority_queue){
     int min;
     min = minimum(min_priority_queue);
-    set_key_i(min_priority_queue, 1, get_key_i(min_priority_queue, lenght(min_priority_queue)));
+    set_value_and_key_i(min_priority_queue, 1, get_value_i(min_priority_queue, lenght(min_priority_queue)), get_key_i(min_priority_queue, lenght(min_priority_queue)));
     decrement_lenght(min_priority_queue);
     min_heapify(min_priority_queue, 1);
     return min;
 };
 
-void decrease_key(MinPriorityQueue *min_priority_queue, int i, int new_value){
-    int aux;
-    if(new_value < get_key_i(min_priority_queue, i)){
-        set_key_i(min_priority_queue, i, new_value);
-        while(i > 1 and get_key_i(min_priority_queue, parent(i)) > get_key_i(min_priority_queue, i)){
-            aux = get_key_i(min_priority_queue, parent(i));
-            set_key_i(min_priority_queue, parent(i), get_key_i(min_priority_queue, i));
-            set_key_i(min_priority_queue, i, aux);
+void decrease_key(MinPriorityQueue *min_priority_queue, int key, int new_value){
+    int i;
+    i = get_position_from_key(min_priority_queue, key);
+    if(new_value < get_value_i(min_priority_queue, i)){
+        set_value_i(min_priority_queue, i, new_value);
+        while(i > 1 and get_value_i(min_priority_queue, parent(i)) > get_value_i(min_priority_queue, i)){
+            swap(min_priority_queue, i, parent(i));
             i = parent(i);
         }
     };
--- estrutura_dados/MinPriorityQueue/c/min_priority_queue_unittest.cc
+++ estrutura_dados/MinPriorityQueue/c/min_priority_queue_unittest.cc
@@ -4,64 +4,64 @@
 TEST(MinPriorityQueue, Minimum){
     MinPriorityQueue min_priority_queue;
     allocate_memory(&min_priority_queue, 9);
-    set_key_i(&min_priority_queue, 1, 1);
-    set_key_i(&min_priority_queue, 2, 2);
-    set_key_i(&min_priority_queue, 3, 3);
-    set_key_i(&min_priority_queue, 4, 19);
-    set_key_i(&min_priority_queue, 5, 17);
-    set_key_i(&min_priority_queue, 6, 7);
-    set_key_i(&min_priority_queue, 7, 25);
-    set_key_i(&min_priority_queue, 8, 36);
-    set_key_i(&min_priority_queue, 9, 100);
-    ASSERT_EQ(1, minimum(&min_priority_queue));
+    set_value_and_key_i(&min_priority_queue, 1, 1, 0);
+    set_value_and_key_i(&min_priority_queue, 2, 2, 1);
+    set_value_and_key_i(&min_priority_queue, 3, 3, 2);
+    set_value_and_key_i(&min_priority_queue, 4, 19, 3);
+    set_value_and_key_i(&min_priority_queue, 5, 17, 4);
+    set_value_and_key_i(&min_priority_queue, 6, 7, 5);
+    set_value_and_key_i(&min_priority_queue, 7, 25, 6);
+    set_value_and_key_i(&min_priority_queue, 8, 36, 7);
+    set_value_and_key_i(&min_priority_queue, 9, 100, 8);
+    ASSERT_EQ(0, minimum(&min_priority_queue));
 }
 
 TEST(MinPriorityQueue, ExtractMinimum){
     MinPriorityQueue min_priority_queue;
     allocate_memory(&min_priority_queue, 9);
-    set_key_i(&min_priority_queue, 1, 1);
-    set_key_i(&min_priority_queue, 2, 2);
-    set_key_i(&min_priority_queue, 3, 3);
-    set_key_i(&min_priority_queue, 4, 19);
-    set_key_i(&min_priority_queue, 5, 17);
-    set_key_i(&min_priority_queue, 6, 7);
-    set_key_i(&min_priority_queue, 7, 25);
-    set_key_i(&min_priority_queue, 8, 36);
-    set_key_i(&min_priority_queue, 9, 100);
-    ASSERT_EQ(1, extract_minimum(&min_priority_queue));
+    set_value_and_key_i(&min_priority_queue, 1, 1, 0);
+    set_value_and_key_i(&min_priority_queue, 2, 2, 1);
+    set_value_and_key_i(&min_priority_queue, 3, 3, 2);
+    set_value_and_key_i(&min_priority_queue, 4, 19, 3);
+    set_value_and_key_i(&min_priority_queue, 5, 17, 4);
+    set_value_and_key_i(&min_priority_queue, 6, 7, 5);
+    set_value_and_key_i(&min_priority_queue, 7, 25, 6);
+    set_value_and_key_i(&min_priority_queue, 8, 36, 7);
+    set_value_and_key_i(&min_priority_queue, 9, 100, 8);
+    ASSERT_EQ(0, extract_minimum(&min_priority_queue));
     ASSERT_EQ(8, lenght(&min_priority_queue));
-    ASSERT_EQ(2, get_key_i(&min_priority_queue, 1));
-    ASSERT_EQ(17, get_key_i(&min_priority_queue, 2));
-    ASSERT_EQ(3, get_key_i(&min_priority_queue, 3));
-    ASSERT_EQ(19, get_key_i(&min_priority_queue, 4));
-    ASSERT_EQ(100, get_key_i(&min_priority_queue, 5));
-    ASSERT_EQ(7, get_key_i(&min_priority_queue, 6));
-    ASSERT_EQ(25, get_key_i(&min_priority_queue, 7));
-    ASSERT_EQ(36, get_key_i(&min_priority_queue, 8));
+    ASSERT_EQ(1, get_key_i(&min_priority_queue, 1));
+    ASSERT_EQ(4, get_key_i(&min_priority_queue, 2));
+    ASSERT_EQ(2, get_key_i(&min_priority_queue, 3));
+    ASSERT_EQ(3, get_key_i(&min_priority_queue, 4));
+    ASSERT_EQ(8, get_key_i(&min_priority_queue, 5));
+    ASSERT_EQ(5, get_key_i(&min_priority_queue, 6));
+    ASSERT_EQ(6, get_key_i(&min_priority_queue, 7));
+    ASSERT_EQ(7, get_key_i(&min_priority_queue, 8));
 }
 
 TEST(MinPriorityQueue, DecreaseKey){
     MinPriorityQueue min_priority_queue;
     allocate_memory(&min_priority_queue, 9);
-    set_key_i(&min_priority_queue, 1, 1);
-    set_key_i(&min_priority_queue, 2, 2);
-    set_key_i(&min_priority_queue, 3, 3);
-    set_key_i(&min_priority_queue, 4, 19);
-    set_key_i(&min_priority_queue, 5, 17);
-    set_key_i(&min_priority_queue, 6, 7);
-    set_key_i(&min_priority_queue, 7, 25);
-    set_key_i(&min_priority_queue, 8, 36);
-    set_key_i(&min_priority_queue, 9, 100);
-    decrease_key(&min_priority_queue, 7, 0);
-    ASSERT_EQ(0, get_key_i(&min_priority_queue, 1));
-    ASSERT_EQ(2, get_key_i(&min_priority_queue, 2));
-    ASSERT_EQ(1, get_key_i(&min_priority_queue, 3));
-    ASSERT_EQ(19, get_key_i(&min_priority_queue, 4));
-    ASSERT_EQ(17, get_key_i(&min_priority_queue, 5));
-    ASSERT_EQ(7, get_key_i(&min_priority_queue, 6));
-    ASSERT_EQ(3, get_key_i(&min_priority_queue, 7));
-    ASSERT_EQ(36, get_key_i(&min_priority_queue, 8));
-    ASSERT_EQ(100, get_key_i(&min_priority_queue, 9));
+    set_value_and_key_i(&min_priority_queue, 1, 1, 0);
+    set_value_and_key_i(&min_priority_queue, 2, 2, 1);
+    set_value_and_key_i(&min_priority_queue, 3, 3, 2);
+    set_value_and_key_i(&min_priority_queue, 4, 19, 3);
+    set_value_and_key_i(&min_priority_queue, 5, 17, 4);
+    set_value_and_key_i(&min_priority_queue, 6, 7, 5);
+    set_value_and_key_i(&min_priority_queue, 7, 25, 6);
+    set_value_and_key_i(&min_priority_queue, 8, 36, 7);
+    set_value_and_key_i(&min_priority_queue, 9, 100, 8);
+    decrease_key(&min_priority_queue, 6, 0);
+    ASSERT_EQ(6, get_key_i(&min_priority_queue, 1));
+    ASSERT_EQ(1, get_key_i(&min_priority_queue, 2));
+    ASSERT_EQ(0, get_key_i(&min_priority_queue, 3));
+    ASSERT_EQ(3, get_key_i(&min_priority_queue, 4));
+    ASSERT_EQ(4, get_key_i(&min_priority_queue, 5));
+    ASSERT_EQ(5, get_key_i(&min_priority_queue, 6));
+    ASSERT_EQ(2, get_key_i(&min_priority_queue, 7));
+    ASSERT_EQ(7, get_key_i(&min_priority_queue, 8));
+    ASSERT_EQ(8, get_key_i(&min_priority_queue, 9));
 }
 
 int main(int argc, char **argv) {
