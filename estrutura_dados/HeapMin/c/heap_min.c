3fc483280cecef2267d097d5f43736f893a15565
João Júnior <joaojunior_ma@gmail.com>
2014-04-06 14:17:08 -0300

    Agora a estrutura HeapMin guarda um vetor de células e não mais um vetor de inteiros

--- estrutura_dados/HeapMin/c/heap_min.c
+++ estrutura_dados/HeapMin/c/heap_min.c
@@ -12,30 +12,38 @@ int right(int i){
 };
 
 void allocate_memory(HeapMin *heap_min, int lenght){
-    heap_min->values = (int *)malloc(sizeof(int) * (lenght + 1));
-    heap_min->values[0] = lenght;
+    heap_min->values = (Cell *)malloc(sizeof(Cell) * (lenght + 1));
+    heap_min->values[0].value = lenght;
 };
 
 int lenght(HeapMin *heap_min){
-    return heap_min->values[0];
+    return heap_min->values[0].value;
 };
 
 void min_heapify(HeapMin *heap_min, int i){
-    int l, r, min, aux;
+    int l, r, min;
     l = left(i);
     r = right(i);
-    if(l <= lenght(heap_min) and heap_min->values[l] < heap_min->values[i])
+    if(l <= lenght(heap_min) and heap_min->values[l].value < heap_min->values[i].value)
         min = l;
     else
         min = i;
-    if(r <= lenght(heap_min) and heap_min->values[r] < heap_min->values[min])
+    if(r <= lenght(heap_min) and heap_min->values[r].value < heap_min->values[min].value)
         min = r;
     if(min != i){
-        aux = heap_min->values[i];
-        heap_min->values[i] = heap_min->values[min];
-        heap_min->values[min] = aux;
+        swap(heap_min, i, min);
         min_heapify(heap_min, min);
     };
+};
+
+void swap(HeapMin *heap_min, int i, int j){
+    int aux_key, aux_value;
+    aux_key = heap_min->values[i].key;
+    aux_value = heap_min->values[i].value;
+    heap_min->values[i].key = heap_min->values[j].key;
+    heap_min->values[i].value = heap_min->values[j].value;
+    heap_min->values[j].key = aux_key;
+    heap_min->values[j].value = aux_value;
 };
 
 void build_min_heap(HeapMin *heap_min){
@@ -44,13 +52,13 @@ void build_min_heap(HeapMin *heap_min){
 };
 
 void decrement_lenght(HeapMin *heap_min){
-    heap_min->values[0] -= 1;
+    heap_min->values[0].value -= 1;
 };
 
 void set_key_i(HeapMin *heap_min, int i, int value){
-    heap_min->values[i] = value;
+    heap_min->values[i].value = value;
 };
 
 int get_key_i(HeapMin *heap_min, int i){
-    return heap_min->values[i];
+    return heap_min->values[i].value;
 };
--- estrutura_dados/HeapMin/c/heap_min.h
+++ estrutura_dados/HeapMin/c/heap_min.h
@@ -1,8 +1,12 @@
 #ifndef HEAP_MIN_H
 #define HEAP_MIN_H
 #include <stdlib.h>
+typedef struct{
+    int key;
+    int value;
+} Cell;
 typedef struct heap_min{
-    int *values;
+    Cell *values;
 } HeapMin;
 
 int parent(int);
@@ -11,6 +15,7 @@ int left(int);
 void allocate_memory(HeapMin *, int);
 int lenght(HeapMin *);
 void min_heapify(HeapMin *, int);
+void swap(HeapMin *, int, int);
 void build_min_heap(HeapMin *);
 void decrement_lenght(HeapMin *);
 void set_key_i(HeapMin *, int, int);
