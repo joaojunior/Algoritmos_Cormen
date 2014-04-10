354231d7e28c05bdccd1d8874b6cb154aabc7040
João Júnior <joao.junior@suzuki.(none)>
2014-04-10 15:48:49 -0300

    Alterando o nome da pasta do algoritmo de Dijkstra

---
+++ algoritmos/DijkstraWithMinPriorityQueue/c/dijkstra.h
@@ -0,0 +1,16 @@
+#ifndef DIJKSTRA_H
+#define DIJKSTRA_H
+#include <stdlib.h>
+#include "../../../estrutura_dados/grafo_matriz_adjacencia/c/graph.h"
+#include "../../../estrutura_dados/MinPriorityQueue/c/min_priority_queue.h"
+#define PREDECESSOR_NULL -1
+typedef struct{
+    int *distance;
+    int *predecessor;
+} DijkstraResult;
+
+DijkstraResult* dijkstra(Graph *, int);
+DijkstraResult* allocate_memory_dijkstra(int);
+void initialize(DijkstraResult*, MinPriorityQueue*, int, int);
+void relax(DijkstraResult*, MinPriorityQueue*, Graph*, int, int);
+#endif
