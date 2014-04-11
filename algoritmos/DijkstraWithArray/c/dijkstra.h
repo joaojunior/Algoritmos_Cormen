71ccee010f047d16d9785afcd7cfbeb7eb70c01f
João Júnior <joaojunior_ma@gmail.com>
2014-04-11 13:49:16 -0300

    Refatorando o Algoritmo de Dijkstra com Array

--- algoritmos/DijkstraWithArray/c/dijkstra.h
+++ algoritmos/DijkstraWithArray/c/dijkstra.h
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include "../../../estrutura_dados/grafo_matriz_adjacencia/c/graph.h"
 #define PREDECESSOR_NULL -1
-#define EMPTY -1
+#define NODE_NOT_VISITED -1
 typedef struct{
     int *distance;
     int *predecessor;
@@ -11,8 +11,8 @@ typedef struct{
 
 DijkstraResult* dijkstra(Graph *, int);
 DijkstraResult* allocate_memory_dijkstra(int);
-void initialize(DijkstraResult*, int, int);
-bool lenght(int *, int);
+void initialize(DijkstraResult*, int*, int, int);
+bool exist_node2visit(int *, int);
 int extract_minimum(int *, int);
 void relax(DijkstraResult*, int*, Graph*, int, int);
 #endif
